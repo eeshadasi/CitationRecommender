1189bbb2d617cd48b6f19892d9c465737c6e2b3d
Eesha dasi <109750147+eeshadasi@users.noreply.github.com>
2024-03-09 09:32:09 +0530

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
 from flask import Flask, render_template, request
 import pickle
 import pandas as pd
 import httpx
 from configparser import ConfigParser
+from textblob import TextBlob  # Import TextBlob for sentiment analysis
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer 
 
 app = Flask(__name__)
 
@@ -49,9 +49,17 @@ def submit():
         results.extend(result['webPages']['value'])
 
     df = pd.DataFrame(results)
-    df = df[['url', 'name']].head()
+    df = df[['url', 'name', 'snippet']].head(10)
 
-    # Save DataFrame as a pickle file
+    # Perform sentiment analysis on combined text of input and snippet
+    sentiments = []
+    for index, row in df.iterrows():
+        combined_text = f"{query} {row['snippet']}"
+        sentiment_score = TextBlob(combined_text).sentiment.polarity
+        sentiments.append(sentiment_score)
+
+    df['sentiment'] = sentiments
+    df = df.sort_values(by='sentiment', ascending=False)
     with open('search_results.pkl', 'wb') as f:
         pickle.dump(df, f)
 
